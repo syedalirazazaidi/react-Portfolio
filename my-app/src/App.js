2b6f09bf74729fb023bdd29c737a9317bf2c668f
ali raza <aaliraza007@hotmail.com>
2019-12-10 21:06:20 +0500

    react portfolio

---
+++ my-app/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Project from './Project';
+import SocialProfiles from './SocialProfile';
+import profile from './assets/profile.png'
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayBio: false
+    };
+  }
+  readMore=()=> {
+    this.setState({ displayBio:!this.state.displayBio })
+  }
+  render() {
+    return (
+      <div>
+        <img src ={profile} alt ='profile' className ="profile"/>
+        <h1>Hi hello</h1>
+        <p>i m raza i did mcs from university if karachi</p>
+        <div>
+          {
+            this.state.displayBio ? (
+              <div>
+                <h1>ali raza as a mern full developre</h1>
+                <button onClick={this.readMore}>Read less</button>
+              </div>
+            ) : (
+                <div><button onClick={this.readMore}>Read More</button></div>
+              )
+          }
+        </div>
+        <hr/>
+        <Project/>
+        <hr/>
+        <SocialProfiles/>
+      </div>
+
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/data/socialProfile.js
@@ -0,0 +1,29 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+
+const SOCIAL_PROFILES = [
+  {
+    id: 1,
+    link: 'mailto:syedaliraza00790@gmail.com',
+    image: emailIcon
+  },
+  {
+    id: 2,
+    link: 'https://github.com/syedalirazazaidi',
+    image: githubIcon
+  },
+  {
+    id: 3,
+    link: 'https://www.linkedin.com/in/aliraza',
+    image: linkedinIcon
+  },
+  {
+    id: 4,
+    link: 'https://twitter.com/raza',
+    image: twitterIcon
+  }
+];
+
+export default SOCIAL_PROFILES; 
